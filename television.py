46e5d91666caeead87f2c380479db89efa3809aa
joushima1 <jaipope2005@gmail.com>
2024-08-17 21:35:50 -0500

    added docstring to init (3)

--- television.py
+++ television.py
@@ -6,7 +6,7 @@ class Television:
 
     def __init__(self) -> None:
         """
-        initializes status, muted, volume, and channel  private variables. Sets both volume and channel to their min
+        initializes status, muted, volume, and channel private variables. Sets both volume and channel to their min
         values.
         """
         self.__status = False
